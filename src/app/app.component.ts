c4312a3a508b18b4a63c7266e66b0bc8506808d5
ADEEL <adeelarifbhatti@gmail.com>
2017-01-25 20:22:36 +0100

    Added some html code/css.And added another component named jobs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import {SkHeaderComponent} from './sk-header.component'
+import {} from './sk-jobs/SkJobsComponent'
 
 @Component({
   selector: 'app-root',
